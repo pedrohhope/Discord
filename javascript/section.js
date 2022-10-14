362072962c15bb4c236274ea85fd1ddc3efdef6d
Tdroid20 <garotorocha90@gmail.com>
2022-10-14 11:29:06 -0300

    FIx: Sytle fixed and icons

--- javascript/section.js
+++ javascript/section.js
@@ -1,16 +1,17 @@
 const target = document.querySelectorAll('[data-anime]')
-const animationClass = 'animate';
+const animationClass = 'animate'
 
-function animeScroll(){
-  const windowTop = window.pageYOffset + 400;
-  target.forEach(function(element) {
-      if((windowTop) > element.offsetTop){
-        element.classList.add(animationClass);
-      }
+function animeScroll() {
+  const windowTop = window.pageYOffset + 400
+  target.forEach(function (element) {
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animationClass)
+    }
 
-      console.log(element.offsetTop);
-
+    console.log(element.offsetTop)
   })
 }
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
+  animeScroll()
+})
